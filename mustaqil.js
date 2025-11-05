b6b4a64a82ae69760522d80b591f68a47c7f5d60
amirbekqaysarov00-maker <amirbekqaysarov00@gmail.com>
2025-11-05 21:33:54 +0500

    frist commit

--- mustaqil.js
+++ mustaqil.js
@@ -1,19 +1,24 @@
-var son1 = +prompt("1-son");
- var amal = prompt("Amal  (+, -, *, /) ");
- var son2 = +prompt("2-son");
- 
- if (amal == "+") {
-     console.log((son1 + son2)); 
- }
- else if (amal == "-") {
-     console.log((son1 - son2));
- }
- else if (amal == "*") {
-     console.log((son1 * son2));
- }
- else if (amal == "/") {
-     console.log((son1 / son2));
- }
- else {
-     console.log(("Noto‘g‘ri amal!"));
- };
+var raqamlar = [];
+var kodlar = ["90","91","93","94","95","97","98","99"];
+
+for (var i = 0; i < 1000; i++) {
+  var kod = kodlar[Math.floor(Math.random() * kodlar.length)];
+  var son = Math.floor(1000000 + Math.random() * 9000000);
+  raqamlar.push(kod + son);
+}
+
+var res = prompt("9 xonali raqam kiriting (masalan: 901234567)");
+
+if (res.length !== 9) {
+  alert(" Raqam 9 xonali bo‘lishi kerak!");
+} 
+else if (res.slice(0, 2) === "92" || res.slice(0, 2) === "96") {
+  alert(" Xato! 92 yoki 96 bilan boshlanadi!");
+} 
+else if (raqamlar.includes(res)) {
+  alert("Raqam ro‘yxatda bor");
+} 
+else {
+  alert("Raqam ro‘yxatda yoq");
+}
+console.log(raqamlar);
